4d6593ee1c559f458aea3f3eaec0dbb9c51d1e22
Kyaw-Zin-George <ktz1695@gmail.com>
2025-05-15 23:41:20 +0100

    Initial Commit

---
+++ Casino_Royale/Casino_RoyaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Casino_RoyaleApp.swift
+//  Casino_Royale
+//
+//  Created by Kyaw Thant Zin(George) on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Casino_RoyaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Casino_Royale/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Casino_Royale
+//
+//  Created by Kyaw Thant Zin(George) on 5/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
