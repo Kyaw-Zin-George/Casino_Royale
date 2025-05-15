d62ec447b4de0ed5afcda0e2982e36753211f79b
Kyaw-Zin-George <ktz1695@gmail.com>
2025-05-15 23:58:14 +0100

    Creating cards model

--- Casino_Royale/Model/CardsModel.swift
+++ Casino_Royale/Model/CardsModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum Suit: String, CaseIterable {
+    case hearts, diamonds, clubs, spades
+}
+
+enum Rank: String, CaseIterable {
+    case two = "2", three = "3", four = "4", five = "5", six = "6"
+    case seven = "7", eight = "8", nine = "9", ten = "10"
+    case jack = "J", queen = "Q", king = "K", ace = "A"
+}
+
+struct Card: Identifiable {
+    let id = UUID()
+    let suit: Suit
+    let rank: Rank
+}
