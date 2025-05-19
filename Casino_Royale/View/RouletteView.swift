cc797cf8532b3f2072489bce74d21c6aff863d53
Kyaw-Zin-George <ktz1695@gmail.com>
2025-05-19 20:22:42 +0100

    Creating roulette view

---
+++ Casino_Royale/View/RouletteView.swift
@@ -0,0 +1,18 @@
+//
+//  RouletteView.swift
+//  Casino_Royale
+//
+//  Created by Kyaw Thant Zin(George) on 5/19/25.
+//
+
+import SwiftUI
+
+struct RouletteView: View {
+    var body: some View {
+        Text("Roulette")
+    }
+}
+
+#Preview {
+    RouletteView()
+}
